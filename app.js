66f2c0034b6c71d58fc7890362253a9acf7b1ffd
Atul Varma <varmaa@gmail.com>
2014-05-20 09:14:41 -0400

    added app.js.

---
+++ app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var app = express();
+var geocodes = {};
+
+var DEBUG = true;
+var PORT = process.env.PORT || 3000;
+var BLOGPOST_URL = 'http://hivenyc.org/2014/04/23/summer-2014-program-opportunities/';
+var GEO_COMPONENT_FILTERS = [
+  'country:US',
+  'administrative_area:NY'
+].join('|');
+
+function getArticleContent(html) {
+  var $ = cheerio.load(html);
+  var article = $('article');
+  $('header, footer, script, .social-share, .tags, ' +
+    '.blog-navigation', article).remove();
+  return article.html();
+}
+
+function geocode(address, cb) {
+  if (address in geocodes)
+    return process.nextTick(cb.bind(null, null, geocodes[address]));
+
+  // https://developers.google.com/maps/documentation/geocoding/
+  request({
+    url: 'http://maps.googleapis.com/maps/api/geocode/json',
+    qs: {
+      address: address,
+      sensor: 'false',
+      components: GEO_COMPONENT_FILTERS
+    }
+  }, function(err, geoRes, body) {
+    if (err) return cb(err);
+    if (geoRes.statusCode != 200)
+      return cb(new Error('got http ' + geoRes.statusCode));
+    body = JSON.parse(body);
+    if (body.status != 'OK') return cb(new Error(body.status));
+    var result = body.results[0];
+    var location = result.geometry.location;
+    location.formatted_address = result.formatted_address;
+    geocodes[address] = location;
+    cb(null, geocodes[address]);
+  });
+}
+
+app.get('/geocode', function(req, res, next) {
+  if (!req.query.q) return next(400);
+
+  geocode(req.query.q.trim(), function(err, info) {
+    if (err) return next(err);
+    return res.send(info);
+  });
+});
+
+app.get('/afterschool-programs.html', function(req, res, next) {
+  request.get(BLOGPOST_URL, function(err, blogRes, body) {
+    if (err) return next(err);
+    if (blogRes.statusCode != 200)
+      return next(new Error('got http ' + blogRes.statusCode));
+    return res.send(getArticleContent(body));
+  });
+});
+
+app.use(express.static(__dirname + '/static'));
+
+app.use(function(err, req, res, next) {
+  if (typeof(err) == 'number')
+    return res.type('text/plain').send(err);
+  if (typeof(err.status) == 'number')
+    return res.type('text/plain').send(err.status, err.message);
+  var stack = err.stack || err.toString();
+  process.stderr.write(stack);
+  res.type('text')
+    .send(500, DEBUG ? stack : 'Sorry, something exploded!');
+});
+
+app.listen(PORT, function() {
+  console.log("listening on port " + PORT);
+});
